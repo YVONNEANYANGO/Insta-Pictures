26bf388c2196824c5fb2fe69c45ddfce6e4c9b21
Yvonne Anyango <ivonnahbonswuh@gmail.com>
2018-12-14 15:00:36 +0300

    create first model for image

--- Instagram/settings.py
+++ Instagram/settings.py
@@ -31,15 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'pics',
-    'bootstrap4',
-    'pics.apps.PicsConfig',
+    'bootstrap3',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'uploads'
 ]
 
 MIDDLEWARE = [
@@ -78,8 +77,10 @@ WSGI_APPLICATION = 'Instagram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'app',
+        'USER' : 'moringaschool',
+        'PASSWORD' : 'vallerryt',
     }
 }
 
@@ -108,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
---
+++ uploads/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Image(models.Model):
+    
+    Image_name = models.CharField(max_length =30) 
+    Image_caption = models.CharField(max_length =30)
+    Likes = models.CharField(max_length =10)
+    Comments = models.CharField(max_length =100)
+    Profile_pic = models.ForeignKey
+    
