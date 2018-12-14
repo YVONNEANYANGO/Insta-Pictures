f7c7762c015610de6a40d5b2eb249308c7c1d5ec
Yvonne Anyango <ivonnahbonswuh@gmail.com>
2018-12-14 16:10:27 +0300

    write tests for image models including the test for save method

--- uploads/models.py
+++ uploads/models.py
@@ -27,6 +27,7 @@ class Image(models.Model):
     Likes = models.CharField(max_length =10)
     Comments = models.CharField(max_length =60)
     Profile = models.ForeignKey(Profile)
+    pub_date = models.DateTimeField(auto_now_add=True)
 
     def save_image(self):
         self.save()
@@ -41,21 +42,4 @@ class Image(models.Model):
         return self.name
 
 
-# class Profile(models.Model):
-#     Profile = models.ImageField(upload_to = 'images/')
-#     Bio = models.CharField(max_length = 500)
 
-#     # Methods
-
-#     def save_profile(self):
-#         self.save()
-
-#     def delete_profile(self):
-#         self.delete()
-
-#     def update_profile(self):
-#         self.update()
-
-#     def __str__(self):
-#         return self.name
-    
--- uploads/tests.py
+++ uploads/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
-
+from .models import Image, Profile
 # Create your tests here.
+
+
+class ImageTestClass(TestCase): 
+
+    # Set up method
+    def setUp(self):
+        self.pic= Image(image = 'sky', Image_Name = 'Test', Image_Caption ='Test Caption', Likes ='Test Like', Comments='Test Comment')
+
+
+    # Testing instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.pic,Image))
+
+
+    # Test Save Method
+    def test_save_method(self):
+        self.pic.save_image()
+        images = Image.objects.all()
+        self.assertTrue(len(editors) > 0)
+
+
+
+
+        
