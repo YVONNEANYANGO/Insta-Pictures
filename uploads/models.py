23e845fc5ea7072edefe22b83d1d4ca7f2663d1c
Yvonne Anyango <ivonnahbonswuh@gmail.com>
2018-12-14 15:16:28 +0300

    add save,delete and update method to the profile model

--- uploads/models.py
+++ uploads/models.py
@@ -2,15 +2,29 @@ from django.db import models
 
 # Create your models here.
 class Image(models.Model):
-    
-    Image_name = models.CharField(max_length =30) 
-    Image_caption = models.CharField(max_length =30)
+    Image = models.ImageField(upload_to = 'images/')
+    Image_Name = models.CharField(max_length =60) 
+    Image_Caption = models.CharField(max_length =30)
     Likes = models.CharField(max_length =10)
     Comments = models.CharField(max_length =100)
-    Profile_pic = models.ForeignKey
+    Profile_Pic = models.ForeignKey(Profile_Pic)
 
 
 class Profile(models.Model):
-    Profile_photo =
+    Profile_photo = models.ImageField(upload_to = 'images/')
     Bio = models.CharField(max_length = 500)
+
+    # Methods
+
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
+
+    def update_profile(self):
+        self.update()
+
+    def __str__(self):
+        return self.profile
     
