70dac0f016d0014969512f3154a6ec7c5f11f45f
Yvonne Anyango <ivonnahbonswuh@gmail.com>
2018-12-14 15:28:14 +0300

    install pillow

--- uploads/models.py
+++ uploads/models.py
@@ -1,17 +1,8 @@
 from django.db import models
 
 # Create your models here.
-class Image(models.Model):
-    Image = models.ImageField(upload_to = 'images/')
-    Image_Name = models.CharField(max_length =60) 
-    Image_Caption = models.CharField(max_length =30)
-    Likes = models.CharField(max_length =10)
-    Comments = models.CharField(max_length =100)
-    Profile_Pic = models.ForeignKey(Profile_Pic)
-
-
 class Profile(models.Model):
-    Profile_photo = models.ImageField(upload_to = 'images/')
+    Profile = models.ImageField(upload_to = 'images/')
     Bio = models.CharField(max_length = 500)
 
     # Methods
@@ -26,5 +17,45 @@ class Profile(models.Model):
         self.update()
 
     def __str__(self):
-        return self.profile
+        return self.name
+
+
+class Image(models.Model):
+    Image = models.ImageField(upload_to = 'images/')
+    Image_Name = models.CharField(max_length =60) 
+    Image_Caption = models.CharField(max_length =60)
+    Likes = models.CharField(max_length =10)
+    Comments = models.CharField(max_length =60)
+    Profile = models.ForeignKey(Profile)
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
+
+    def update_image(self):
+        self.update()
+
+    def __str__(self):
+        return self.name
+
+
+# class Profile(models.Model):
+#     Profile = models.ImageField(upload_to = 'images/')
+#     Bio = models.CharField(max_length = 500)
+
+#     # Methods
+
+#     def save_profile(self):
+#         self.save()
+
+#     def delete_profile(self):
+#         self.delete()
+
+#     def update_profile(self):
+#         self.update()
+
+#     def __str__(self):
+#         return self.name
     
