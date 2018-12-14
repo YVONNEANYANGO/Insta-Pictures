8feabd2e662c23c64da71546b1cbfed2b3e51953
Yvonne Anyango <ivonnahbonswuh@gmail.com>
2018-12-14 16:44:16 +0300

    create profile test and added a teardown method to help delete objects once they have been saved

--- uploads/admin.py
+++ uploads/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from .models import Image, Profile
 
+class ImageAdmin(admin.ModelAdmin):
+    filter_horizontal =('Profile') 
+
 # Register your models here.
 admin.site.register(Image)
 admin.site.register(Profile)
--- uploads/tests.py
+++ uploads/tests.py
@@ -7,9 +7,16 @@ class ImageTestClass(TestCase):
 
     # Set up method
     def setUp(self):
+
+        # Creating profile and saving it
+        self.kim= Profile(profile = 'photo', bio = 'Test Bio Data')
+        self.kim.save_profile()
+
+        # Creating new image and saving it
         self.pic= Image(image = 'sky', Image_Name = 'Test', Image_Caption ='Test Caption', Likes ='Test Like', Comments='Test Comment')
+        self.pic.save_image()
 
-
+   
     # Testing instance
     def test_instance(self):
         self.assertTrue(isinstance(self.pic,Image))
@@ -20,6 +27,10 @@ class ImageTestClass(TestCase):
         self.pic.save_image()
         images = Image.objects.all()
         self.assertTrue(len(editors) > 0)
+
+    def tearDown(self):
+        Profile.objects.all().delete()
+        Image.objects.all().delete()
 
 
 
