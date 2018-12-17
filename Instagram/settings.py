9c373df05aea0f2fd08810bb2b4fd09083478d2a
Yvonne Anyango <ivonnahbonswuh@gmail.com>
2018-12-17 11:37:40 +0300

    Define the email function in email.py file

--- Instagram/settings.py
+++ Instagram/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -85,6 +86,12 @@ DATABASES = {
     }
 }
 
+# Email configurations remember to install python-decouple
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'ivonnahbonswuh@gmail.com'
+EMAIL_HOST_PASSWORD = 'valqueen'
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
